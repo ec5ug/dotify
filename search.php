cde1a9d9dde364705c337e269af3396c8765de0d
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2024-04-05 09:54:00 -0400

    Added playlist button that enables/disables if you have at least one playlist created (otherwise not yet functional).

--- search.php
+++ search.php
@@ -41,6 +41,49 @@ if(isset($_POST["search-submit"])){
         <input type="text" name="search" style="width: 300px;" placeholder="Search by title, artist, or release year" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search for a song by title, artist, or release year'">
         <input type="submit" name="search-submit">
     </form>
+    <style>
+        /* Tooltip container */
+        .tooltip {
+            position: relative;
+            display: inline-block;
+        }
+
+        /* Tooltip text */
+        .tooltip .tooltiptext {
+            visibility: hidden;
+            width: 120px;
+            background-color: #555;
+            color: #fff;
+            text-align: center;
+            border-radius: 6px;
+            padding: 5px;
+            position: absolute;
+            z-index: 1;
+            bottom: 125%;
+            left: 50%;
+            margin-left: -60px;
+            opacity: 0;
+            transition: opacity 0.3s;
+        }
+
+        /* Tooltip arrow */
+        .tooltip .tooltiptext::after {
+            content: "";
+            position: absolute;
+            top: 100%;
+            left: 50%;
+            margin-left: -5px;
+            border-width: 5px;
+            border-style: solid;
+            border-color: #555 transparent transparent transparent;
+        }
+
+        /* Show the tooltip text when hovering over the tooltip container */
+        .tooltip:hover .tooltiptext {
+            visibility: visible;
+            opacity: 1;
+        }
+    </style>
 </header>
 
 <?php
@@ -84,6 +127,17 @@ if (!empty($songs_found)) {
             } else {
                 echo "<input type='hidden' name='song_id' value='" . $song_found['song_id'] . "'>";
                 echo "<button type='submit' name='favorite-create'>Add to Favorites</button>";
+            }
+            echo "</td>";
+            echo "<td>";
+            if (getPlaylist($username)){
+                echo "<button type='submit' name='#'>Add to Playlist</button>";
+            }
+            else{
+                echo "<div class='tooltip'>";
+                echo "<button type='button' disabled style='color: gray; cursor: not-allowed;'>Add to Playlist</button> ";
+                echo "<span class='tooltiptext'>You must create at least one playlist first!</span>";
+                echo "</div>";
             }
             echo "</form>";
             echo "</td>";
