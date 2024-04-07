7aa8e4acfefb469d0fa349c3bb26e075549b7f99
ec5ug <ec5ug@virginia.edu>
2024-04-07 16:56:38 -0400

    added Reccomendations page. No backend yet

---
+++ reccomendations.php
@@ -0,0 +1,22 @@
+<?php
+require "import.php";
+require "logged-in.php";
+?>
+<!DOCTYPE html>
+ <html lang="en">
+     <head>
+         <meta charset="utf-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1"> 
+         <meta name="description" content="">
+         <meta name="keywords" content="">
+
+         <link rel="stylesheet" href="styles/main.css">
+     </head>  
+     <body>
+        <?php include 'nav_bar.php'; ?>
+        <h1>Reccomended Songs</h1>
+        <h2>Songs sung by artists that you have listened to</h2>
+        <h2>Songs sung with similar energy levels</h2> 
+     </body>
+ </html>
